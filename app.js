9f0904f1dcc44d3b2815e85bdb36b65eb36a2d95
rivalcoba <ivan.rivalcoba@gmail.com>
2024-05-06 09:37:21 -0500

    🌟 FEAT: Simple Color flipper simple Done

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+// Get the button element
+const btn = document.getElementById("btn");
+// Get the color element
+const color = document.querySelector(".color");
+
+// Add event listener to the button
+btn.addEventListener("click", ()=>{
+  // Obtener un número aleatorio entre 0 y 3
+  const randomNumber = getRandomNumber();
+  // Seleccionado un elemento
+  // del arreglo colors
+  const colorPicked = colors[randomNumber];
+  // Cambiar el color del body
+  document.body.style.backgroundColor = colorPicked;
+  // Cambiar el color del texto
+  color.textContent = colorPicked;
+});
+
+// Función para obtener un número aleatorio
+function getRandomNumber(){
+  return Math.floor(Math.random() * colors.length);
+}
